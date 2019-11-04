034d4138e67dde1655c0aca16c9b68deb8900c32
Kamshat Mussina <mussinak777@gmail.com>
2019-11-03 21:07:39 -0700

    added buttons in score file, styles

--- questions.js
+++ questions.js
@@ -1,4 +1,5 @@
 //questions stored as an object
+
 var questionContainerElement = document.getElementById("question-container");
 var startBtn = document.getElementById("start-btn");
 var questionElement = document.getElementById("question");
@@ -11,29 +12,29 @@ var score = 0;
 var counter = 0;
 var finishedTime = 0;
 
-
+window.onload= function () {
 var questions = [
   {
-    question: "How do  you create a function in Javascript",
+    question: "1. How do  you create a function in Javascript ?",
     choices: ["1. function = myFunction()", "2. function:my Function()", "3. function myFunction()", "4. function-myFunction()"],
-    answer: "3. function myFunction()"
+    answer: "3. function myFunction()",
   },
   {
-    question: "How does a FOR loop start ?",
+    question: "2. How does a FOR loop start ?",
       choices: ["1. for(i <=7; i++)", "2. for (i = 0; i <=7; i++)", "3. for(i = 0; i <= 5)", "4. for i = 1 to 7"],
-      answer: "2. for (i = 0; i <=7; i++)"
+      answer: "2. for (i = 0; i <=7; i++)",
     }, 
-    { question: "Which event occurs when the user clicks on an HTML element ?",
+    { question: "3. Which event occurs when the user clicks on an HTML element ?",
       choices: ["1. onclick", "2. onmouseclick", "3. onmouseover", "4. onchange"],
-      answer: "1. onclick"
+      answer: "1. onclick",
     },
-    { question: "How do you declare a Javascript variable ?",
+    { question: "4. How do you declare a Javascript variable ?",
     choices: ["1. variable carName", "2. v carName", "3. var carName", "4. var CarName"],
-    answer: "3. var carName"
+    answer: "3. var carName",
   },
-  { question: "How do you write 'Hello World' in an alert box ?",
+  { question: "5. How do you write 'Hello World' in an alert box ?",
   choices: ["1. msgBox('Hello World)", "2. alertBox('Hello World')", "3. alert('Hello World')", "4. msg('Hello World')"],
-  answer: "3. alert('Hello World')"
+  answer: "3. alert('Hello World')",
 },
 ];
 
@@ -42,7 +43,8 @@ startBtn.addEventListener("click", startQuiz);
 document.getElementById('finished').style.display = "none";
 
 function startQuiz () {
-  startBtn.style.visibility = "hidden";
+  event.preventDefault();
+  document.getElementById("hide-this").style.visibility = "hidden";
   questionContainerElement.removeAttribute("class", "hide");
   displayAnswers();
   setTime();
@@ -51,7 +53,7 @@ function startQuiz () {
 function setTime () {
 var timer = setInterval(function() {
    count--;
-   document.getElementById("timer").textContent = count;
+   document.getElementById("timer").textContent = "Time: " + count;
    if (count === 0) {
    clearInterval(timer)
    document.getElementById("timer").textContent = "Time is out!";
@@ -85,7 +87,6 @@ answerElement.addEventListener("click", function() {
       checkGameOver();
     }
     shuffledQuestions();
-    checkGameOver();
   
  
 });
@@ -104,18 +105,20 @@ function checkGameOver(){
     questionContainerElement.style.display = "none";
     document.getElementById("timer").style.display = "none";
     document.getElementById('finished').style.display = "block";
+    var user = localStorage.getItem("initials");
+    console.log(user);
     document.getElementById("submit").addEventListener("click", function() {
+      event.preventDefault();
       var initials = document.getElementById("initials").value
       localStorage.setItem(initials, finishedTime);
       document.getElementById("final-score").textContent = finishedTime;
-      document.getElementById("show-score").innerText = localStorage.getItem(initials);
+     
     })
-    
+   
     finishedTime = count;
   }
 }
-
-
+}
 
 
 
